020b07e602b8684d0be56d7c4fcb6909522e72dd
Juan Sebastian Mejia Ortiz <juan.mejiao1@udea.edu.co>
2025-03-24 18:53:08 -0500

    Format changes for python linter to be happy (mostly)

--- src/phasorialDiagrams.py
+++ src/phasorialDiagrams.py
@@ -11,14 +11,14 @@ ax = plt.subplot(111, projection='polar')
 
 # Plot each phasor
 for phasor in phasors:
-    ax.plot([0, phasor['angle']],
+    plt.plot([0, phasor['angle']],
             [0, phasor['magnitude']], 
             linewidth=3, 
             color=phasor['color'],
             label=phasor['label'])
 
     # Add marker at phasor tip
-    ax.plot(phasor['angle'],
+    plt.plot(phasor['angle'],
             phasor['magnitude'], 
             marker='o', 
             markersize=8, 
@@ -30,28 +30,12 @@ ax.set_yticks(list(range(0, 7, 1)))
 # Set r label position
 ax.set_rlabel_position(0)
 
-# Grid and labels customization
-ax.grid(True, linestyle='--', alpha=0.7)
+# Grid
+plt.grid(True, linestyle='--', alpha=0.7)
 
-# Legend
-ax.legend(loc='upper right', bbox_to_anchor=(1.3, 1.1))
-
-# Title
+# Title and Legend
 plt.title('Phasor Diagram', pad=20)
+plt.legend(loc='upper right', bbox_to_anchor=(1.3, 1.1))
 
 # Save figure
-plt.savefig('./bin/polar.png', dpi=300, bbox_inches='tight')
-
-# Grid and labels customization
-# ax.set_theta_zero_location('E')  # 0° points East
-# ax.set_theta_direction(-1)       # Clockwise angle increase
-# ax.set_rlabel_position(0)        # Radial labels at 0°
-# ax.set_yticks(np.arange(0, 7, 1))  # Radial ticks
-# ax.grid(True, linestyle='--', alpha=0.7)
-#
-# # Title and legend
-# plt.title('Phasor Diagram', pad=20)
-# ax.legend(loc='upper right', bbox_to_anchor=(1.3, 1.1))
-#
-# # Save figure
-# plt.savefig('./bin/polar.png', dpi=300, bbox_inches='tight')
+plt.savefig('./bin/img/polar.png', dpi=300, bbox_inches='tight')
